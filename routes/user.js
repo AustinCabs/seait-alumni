7199234d15047bc7f7ed7a32de6c00fb7ef5c80a
Aus_Cabs <46185867+AustinCabs@users.noreply.github.com>
2021-11-30 23:59:36 +0800

    yearbooks

--- routes/user.js
+++ routes/user.js
@@ -17,9 +17,24 @@ router.get('/user', async(req,res) =>{
             const data = await sequelize.query(q2, { type: QueryTypes.SELECT })
             let avatar =  req.session.name
             //  console.log(data);
+
+            let data_courses = {}
+            const courses = await sequelize.query("SELECT * FROM courses ", { type: QueryTypes.SELECT });
+            
+            let year = new Date().getFullYear();
+            let arr_year = [];
+            for (let index = 2005; index < year; index++) {
+                arr_year.push(index);
+            }
+            arr_year.push(year);
+            data.courses = courses;
+            data.years = arr_year;
+
             res.render('admin/user',{
                 avatar : avatar.toUpperCase(),
-                data:data
+                data:data,
+                courses:courses,
+                years:arr_year
             });
     
         } catch (e) {
@@ -31,6 +46,65 @@ router.get('/user', async(req,res) =>{
         res.redirect('/');
     }
 });
+
+router.get('/users/:id', async(req,res) =>{
+        try {
+            let sql = `select * from alumnis where alumni_id = ${req.params.id}`
+            const q = await sequelize.query(sql, { type: QueryTypes.SELECT });
+            console.log(q)
+             
+            return  res.send(q[0])
+
+        } catch (e) {
+            console.log(`error : ${e}`);
+        }
+})
+
+router.put('/users/:id',async(req,res) => {
+     try {
+         let {
+            user_id,
+            fname,
+            mname,
+            lname,
+            bday,
+            phone,
+            year,
+            gender,
+            course,
+            job_status
+         } = req.body
+
+        let sql = `update alumnis set fname="${fname}" ,mname="${mname}", lname="${lname}", bday='${bday}' ,gender="${gender}" , phone_num="${phone}", year_graduated="${year}", course_id=${course}, job_status="${job_status}" where alumni_id = ${user_id}`
+        const q = await sequelize.query(sql, { type: QueryTypes.UPDATE });
+        console.log(q)
+        res.send({success:true})
+    } catch (e) {
+        console.log(e)
+        res.send({success:false,message:e})
+        
+     }
+})
+
+router.delete('/users/:id',async(req,res) => {
+    try {
+        let sql = `delete  from alumnis where alumni_id = ${req.params.id}`
+        const q = await sequelize.query(sql, { type: QueryTypes.DELETE });
+        console.log(q)
+
+        let sql1 = `delete  from users where alumni_id = ${req.params.id}`
+        const q1 = await sequelize.query(sql1, { type: QueryTypes.DELETE });
+        console.log(q1)
+
+         res.send({success:true})
+         
+        } catch (e) {
+            console.log(e);
+            res.send({success:false,message:e})
+            
+}
+})
+
 
 
 module.exports = router;
--- routes/yearbook.js
+++ routes/yearbook.js
@@ -42,26 +42,7 @@ router.get('/yearbook',async(req,res) => {
          avatar : avatar.toUpperCase()
       });
    } else {
-    
-   // let data = {}
-   // const courses = await sequelize.query("SELECT * FROM courses ", { type: QueryTypes.SELECT });
-   
-   // let year = new Date().getFullYear();
-   // let arr_year = [];
-   // for (let index = 2005; index < year; index++) {
-   //      arr_year.push(index);
-   // }
-   // arr_year.push(year);
-   // data.courses = courses;
-   // data.years = arr_year;
-   // // console.log(data);
-
-   // res.render('alumni/login',{
-   //      courses:courses,
-   //      years:arr_year
-   // });
-
-   res.redirect('/');
+      res.redirect('/');
 
    }
 });
@@ -69,6 +50,61 @@ router.get('/yearbook',async(req,res) => {
 router.get('/yearbook-detail',(req,res) => {
    res.render('alumni/yearbook-details');
 });
+
+router.get('/manage-yearbooks',async(req,res) => {
+
+if (req.session.user_id != null) {
+   try {
+
+      const courses = await sequelize.query("SELECT * FROM courses ", { type: QueryTypes.SELECT });
+      let  sql  = `select year_graduated,yearbook_id,course_name 
+      from yearbooks
+      inner join courses on yearbooks.course_id = courses.course_id`
+      const data = await sequelize.query(sql, { type: QueryTypes.SELECT });
+      
+      let year = new Date().getFullYear();
+      let arr_year = [];
+      for (let index = 2005; index < year; index++) {
+           arr_year.push(index);
+      }
+      arr_year.push(year);
+
+      let avatar =  req.session.name
+      res.render('admin/manage-yearbook',{
+         yearbooks:data,
+         courses:courses,  
+         years:arr_year,
+         avatar : avatar.toUpperCase()
+      });
+      
+   } catch (e) {
+      console.log(e);
+      res.send({success:false})
+   }
+
+} else {
+   res.redirect('/');
+
+}
+ 
+});
+
+router.post('/manage-yearbooks',async(req,res) => {
+    try {
+   
+   let {year,course} =  req.body;
+
+    let sql = `insert into yearbooks (course_id,year_graduated) values (${course},"${year}")`
+    const q = await sequelize.query(sql, { type: QueryTypes.INSERT });
+    res.send({success:true})
+
+    } catch (e) {
+      console.log(e);
+      res.send({success:false})
+    }
+})
+
+ 
 
 
  module.exports = router;
